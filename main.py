25d5af0d3f91b283bc2a9e6b50ae5b6b8e8e6a2b
Radomir Dopieralski <openstack@sheep.art.pl>
2019-03-30 16:39:21 +0100

    Simplify display and improve input

--- main.py
+++ main.py
@@ -9,23 +9,15 @@ DX = 0, 1, 0, -1
 DY = -1, 0, 1, 0
 
 
-screen = displayio.Group()
-board.DISPLAY.show(screen)
-image = displayio.Bitmap(8, 32, 4)
-for y in range(8):
-    for x in range(8):
-        image[x, y] = 0
-        image[x, y + 8] = 1
-        image[x, y + 16] = 2
-        image[x, y + 24] = 3
+group = displayio.Group(scale=8)
+board.DISPLAY.show(group)
+screen = displayio.Bitmap(16, 16, 4)
 palette = displayio.Palette(4)
 palette[0] = 0x111111
 palette[1] = 0xaa0099
 palette[2] = 0x22aa00
 palette[3] = 0xee00bb
-grid = displayio.TileGrid(image, pixel_shader=palette, width=16, height=16,
-    tile_width=8, tile_height=8, x=0, y=0)
-screen.append(grid)
+group.append(displayio.TileGrid(screen, pixel_shader=palette))
 left = touchio.TouchIn(board.A5)
 right = touchio.TouchIn(board.A2)
 
@@ -35,32 +27,34 @@ facing = 0
 size = 3
 apple = 8, 4
 
-
 body = [head]
-grid[apple] = 2
+screen[apple] = 2
 tick = time.monotonic()
 while True:
-    if left.value:
+    turn_left = False
+    turn_right = False
+    tick += 0.5
+    while True:
+        time.sleep(0.05)
+        turn_left = turn_left or left.value
+        turn_right = turn_right or right.value
+        if tick <= time.monotonic():
+            break
+    if turn_left:
         facing = (facing - 1) % 4
-    if right.value:
+    if turn_right:
         facing = (facing + 1) % 4
-    grid[head] = 1
+    screen[head] = 1
     head = (head[0] + DX[facing]) % 16, (head[1] + DY[facing]) % 16
     if head in body:
         break
     body.append(head)
-    grid[head] = 3
+    screen[head] = 3
     if head == apple:
         size += 1
         apple = random.randint(0, 15), random.randint(0, 15)
-        grid[apple] = 2
+        screen[apple] = 2
     if len(body) > size:
         tail = body.pop(0)
-        grid[tail] = 0
+        screen[tail] = 0
     board.DISPLAY.refresh_soon()
-    now = time.monotonic()
-    tick += 0.5
-    if tick < now:
-        tick = now
-    else:
-        time.sleep(tick - now)
